e1ca8918bdb5d0b4d9208f9fcaf94e2dbdac120a
hakbrk <jhjhawkins@gmail.com>
2019-09-26 18:20:27 -0500

    removed unwanted files

--- Belly_Button_Diversity/static/js/bonus.js
+++
@@ -1,9 +0,0 @@
-function buildGauge(sample) {
-    var url = "/metadata/"+sample;
-
-    d3.json(url).then(function(response) {
-        let list = d3.select("#gauge");
-        list.html("");
-        console.log("Will this show up?");
-              });
-};
